698bf0da444af780d6f69df753f6971e33dddc0c
Yang Xiao <104522189+NKUShaw@users.noreply.github.com>
2024-03-25 16:25:33 +0800

    Add files via upload

--- utils/get_datasets.py
+++ utils/get_datasets.py
@@ -6,8 +6,9 @@ def get_dataset(args):
     if args.dataset == 'cifar':
         data_dir = './data/{}'.format(args.dataset)
         apply_transform = transforms.Compose(
-            [transforms.ToTensor(),
-             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+            [transforms.Grayscale(num_output_channels=1),
+             transforms.ToTensor(),  # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+             transforms.Normalize((0.5,), (0.5,))])
         train_dataset = datasets.CIFAR10(data_dir, train=True, download=True,
                                          transform=apply_transform)
         test_dataset = datasets.CIFAR10(data_dir, train=False, download=True,
@@ -32,6 +33,28 @@ def get_dataset(args):
                                               transform=apply_transform)
         test_dataset = datasets.MNIST(data_dir, train=False, download=True,
                                              transform=apply_transform)
+    elif args.dataset == 'cifar100':
+        data_dir = './data/{}'.format(args.dataset)
+        apply_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(15),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        train_dataset = datasets.CIFAR100(data_dir, train=True, download=True,
+                                              transform=apply_transform)
+        test_dataset = datasets.CIFAR100(data_dir, train=False, download=True,
+                                             transform=apply_transform)
+    elif args.dataset == 'caltech101':
+        data_dir = './data/{}'.format(args.dataset)
+        apply_transform = transforms.Compose(
+            [transforms.ToPILImage(),
+             transforms.Resize((256, 256)),
+             transforms.ToTensor(),
+             transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                  std=[0.229, 0.224, 0.225])])
+        train_dataset = datasets.Caltech101(data_dir, train=True, transform=apply_transform, download=True)
+        test_dataset = datasets.Caltech101(data_dir, train=False, transform=apply_transform, download=True)
 
     else:
         print('暂时没加这个数据集')
--- utils/get_losses.py
+++ utils/get_losses.py
@@ -9,7 +9,7 @@ class CapsuleLoss(nn.Module):
         self.upper = upper_bound
         self.lower = lower_bound
         self.lmda = lmda
-        self.reconstruction_loss_scalar = 5e-4
+        self.reconstruction_loss_scalar = 5e-3
         self.mse = nn.MSELoss(reduction='sum')
 
     def forward(self, images, labels, logits, reconstructions):
@@ -17,4 +17,27 @@ class CapsuleLoss(nn.Module):
         right = (logits - self.lower).relu() ** 2  # False positive
         margin_loss = torch.sum(labels * left) + self.lmda * torch.sum((1 - labels) * right)
         reconstruction_loss = self.mse(reconstructions.contiguous().view(images.shape), images)
+        self.margin_loss = margin_loss
+        self.reconstruction_loss = reconstruction_loss
+        # Orthogonal loss : Cosine similarity between filters / Or maximize L2 distance between filters
         return margin_loss + self.reconstruction_loss_scalar * reconstruction_loss
+
+class OrthogonalProjectionLoss(nn.Module):
+    def __init__(self, gamma=1.0):
+        super(OrthogonalProjectionLoss, self).__init__()
+        self.gamma = gamma
+
+    def forward(self, features, labels=None):
+        device = (torch.device('cuda') if features.is_cuda else torch.device('cpu'))
+        #  features are normalized
+        features = F.normalize(features, p=2, dim=1)
+        labels = labels[:, None]  # extend dim
+        mask = torch.eq(labels, labels.t()).bool().to(device)
+        eye = torch.eye(mask.shape[0], mask.shape[1]).bool().to(device)
+        mask_pos = mask.masked_fill(eye, 0).float()
+        mask_neg = (~mask).float()
+        dot_prod = torch.matmul(features, features.t())
+        pos_pairs_mean = (mask_pos * dot_prod).sum() / (mask_pos.sum() + 1e-6)
+        neg_pairs_mean = (mask_neg * dot_prod).sum() / (mask_neg.sum() + 1e-6)  # TODO: removed abs
+        loss = (1.0 - pos_pairs_mean) + self.gamma * neg_pairs_mean
+        return loss
